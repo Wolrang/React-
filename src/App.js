456979b36689c478df4d562251cac351f904bffb
Wolrang <dkfud252@naver.com>
2023-03-22 14:53:30 +0900

    🎉  React-Shoppingmall

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+/* eslint-disable */
 import './App.css';
+import {Routes, Route, Link} from 'react-router-dom';
+import Login from './page/Login'
+import ProductAll from './page/ProductAll'
+import ProductDetail from './page/ProductDetail'
+import Nav from './component/Nav'
+
+// 1. 전체상품(ProductAll), 로그인(Login), 상세페이지(ProductDetail)
+// 2. 로그인 전에 상세페이지 접속 시, 로그인 페이지를 먼저 보여준다.
+// 3. 로그인이 이미 되어있다면, 상세페이지 보여준다.
+// 4. 로그인 상태면 로그아웃 버튼이 보이고, 로그아웃 하면 로그인 버튼이 보인다. 
 
 function App() {
   return (
     <div className="App">
-   
+      <Nav></Nav>
+      <Routes>
+        <Route path='' element={<ProductAll></ProductAll>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/product/:id' element={<ProductDetail></ProductDetail>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/component/Nav.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import nike from './images/nike.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
+import { faBagShopping } from '@fortawesome/free-solid-svg-icons'
+
+const Nav = () => {
+  const loginList = ['매장찾기', '고객센터', '가입하기', '로그인']
+  const menuList = ['New Releases', 'Men', 'Women', 'Kids', 'Sale', 'SNKRS', '나이키 앱']
+  return (
+    <div>
+      <div className='login'>
+        <ul className='nav'>
+          {
+            loginList.map((item, i) => {
+              return (
+                <li className='login-item' key={i}>{item}</li>
+              )
+            })
+          }
+        </ul>
+
+        <div className="gnb">
+          <img src={nike} alt='nike-logo'></img>
+          <ul className='gnb-list'>
+            {
+              menuList.map((menu, i) => {
+                return (
+                  <li className='gnb-item' key={i}>{menu}</li>
+                )
+              })
+            }
+          </ul>
+          <div className='icon-list'>
+            <div className='search'>
+              <FontAwesomeIcon className='glass' icon={faMagnifyingGlass} />
+              <input type='text' placeholder='검색'></input>
+            </div>
+            <div>
+              <FontAwesomeIcon className='heart' icon={faHeart} />
+              <FontAwesomeIcon className='shopping' icon={faBagShopping} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
