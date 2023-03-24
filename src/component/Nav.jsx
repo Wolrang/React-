6f9d8fd426e61af83528027237ef42b00a942e94
Wolrang <dkfud252@naver.com>
2023-03-24 18:31:09 +0900

    🎉  React-Shoppingmall

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import nike from './images/nike.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { faHeart } from '@fortawesome/free-regular-svg-icons'
 import { faBagShopping } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from 'react-router-dom'
+import ProductCard from './ProductCard'
 
 const Nav = () => {
   const loginList = ['매장찾기', '고객센터', '가입하기', '로그인']
@@ -14,6 +15,42 @@ const Nav = () => {
   const goToLogin = () => {
     navigate('/Login')
   }
+
+  // onkeyPress 유저가 키를 눌렀을 때 발생한다.
+  // alt, ctrl, shift, ESC 키에는 onkeyPress가 적용안됨
+  // Enter (조건문)
+  
+  const [searching, setSearching] = useState("")
+  const onChange = (e) => {
+    setSearching(e.target.value.toLowerCase());
+  }
+  
+  const search = (e) => {
+    if (e.key === "Enter") {
+      console.log(searching)
+    }
+  }
+
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    let url = " http://localhost:5000/products"
+    let response = await fetch(url)
+    let data = await response.json()
+    console.log(data)
+
+    setProducts(data)
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+
+  const searched = products.filter((item) =>
+    item.title.toLowerCase().includes(searching)
+    );
+    console.log(searched)
 
   return (
     <div>
@@ -48,7 +85,7 @@ const Nav = () => {
           <div className='icon-list'>
             <div className='search'>
               <FontAwesomeIcon className='glass' icon={faMagnifyingGlass} />
-              <input type='text' placeholder='검색'></input>
+              <input type='text' placeholder='검색' onKeyPress={search} value={searching} onChange={onChange}></input>
             </div>
             <div>
               <FontAwesomeIcon className='heart' icon={faHeart} />
